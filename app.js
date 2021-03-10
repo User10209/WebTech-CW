bb0db6afc11c7867cf1c6c926221daaadc0fc74f
Mukhammadjon Ergashev <00010209@wiut.uz>
2021-03-10 17:52:28 +0500

    add views

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+
+
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+	res.render('home')
+
+})
+
+app.listen(8000)
