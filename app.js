91c353037a44bcd5d7fe86412621e0eda51fb3fa
Mukhammadjon Ergashev <00010209@wiut.uz>
2021-03-17 17:17:33 +0500

    created some new pages

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
 	res.render('home')
-
 })
 
-app.listen(8000)
+app.get('/create', (req,res) => {
+	res.render('create')
+})
+
+app.get('/notes', (req,res) => {
+	res.render('notes', { noteList: ['First', 'Second']})
+})
+
+app.get('/notes/detail', (req,res)=> {
+	res.render('detail')
+})
+
+app.listen(8000, err=> {
+	if (err) console.log(err)
+	console.log('App is running on port 8000....')
+})
